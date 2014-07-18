5c26bb4683234e850b52c46ad987237678faf3f5
KururuLABO <nxeapher@hotmail.com>
2014-07-18 07:27:35 +0700

    Add export function and rewrite code

--- ScriptForm.cs
+++ ScriptForm.cs
@@ -15,6 +15,7 @@ namespace MapleShark
     {
         private string mPath = "";
         private MaplePacket mPacket = null;
+        private StructureForm mStructureForm = new StructureForm();
 
         public ScriptForm(string pPath, MaplePacket pPacket)
         {
@@ -28,23 +29,29 @@ namespace MapleShark
         }
 
         internal MaplePacket Packet { get { return mPacket; } }
+        public StructureForm StructureForm { get { return mStructureForm; } }
 
         private void ScriptForm_Load(object pSender, EventArgs pArgs)
         {
             mScriptEditor.Document.SetSyntaxFromEmbeddedResource(Assembly.GetExecutingAssembly(), "MapleShark.ScriptSyntax.txt");
             if (File.Exists(mPath)) mScriptEditor.Open(mPath);
-        }
 
-        private void mScriptEditor_TextChanged(object pSender, EventArgs pArgs)
-        {
-            mSaveButton.Enabled = true;
         }
 
         private void mSaveButton_Click(object pSender, EventArgs pArgs)
         {
             if (mScriptEditor.Document.Text.Length == 0) File.Delete(mPath);
             else mScriptEditor.Save(mPath);
-            Close();
+            //Close();
+            //Save and refresh structure without closing form By EventBackColorChanged
+            if (this.BackColor == SystemColors.ButtonFace)
+            {
+                this.BackColor = SystemColors.Control;
+            }
+            else
+            {
+                this.BackColor = SystemColors.ButtonFace;
+            }
         }
 
         private void mImportButton_Click(object sender, EventArgs e)
@@ -58,6 +65,113 @@ namespace MapleShark
                         return;
                     mScriptEditor.Open(FileImporter.FileName);
                 }
+            }
+        }
+
+
+        private void importScriptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (FileImporter.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(FileImporter.FileName))
+                {
+                    if (mScriptEditor.Document.Text.Length > 0 && MessageBox.Show("Are you sure you want to open this file? The current script will be replaced with the one from the file you selected.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                        return;
+                    mScriptEditor.Open(FileImporter.FileName);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mScriptEditor.Document.Text.Length == 0) File.Delete(mPath);
+            else mScriptEditor.Save(mPath);
+            //Close();
+            //Save and refresh structure without closing form By EventBackColorChanged
+            if (this.BackColor == SystemColors.ButtonFace)
+            {
+                this.BackColor = SystemColors.Control;
+            }
+            else
+            {
+                this.BackColor = SystemColors.ButtonFace;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tutorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImportButton_Click(object sender, EventArgs e)
+        {
+
+            if (FileImporter.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(FileImporter.FileName))
+                {
+                    if (mScriptEditor.Document.Text.Length > 0 && MessageBox.Show("Are you sure you want to open this file? The current script will be replaced with the one from the file you selected.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                        return;
+                    mScriptEditor.Open(FileImporter.FileName);
+                }
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (mScriptEditor.Document.Text.Length == 0) File.Delete(mPath);
+            else mScriptEditor.Save(mPath);
+            //Close();
+            //Save and refresh structure without closing form By EventBackColorChanged
+            if (this.BackColor == SystemColors.ButtonFace)
+            {
+                this.BackColor = SystemColors.Control;
+            }
+            else
+            {
+                this.BackColor = SystemColors.ButtonFace;
+            }
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            ExportScript.InitialDirectory = @"C:\";
+            ExportScript.Title = "Expoer Mapleshark script";
+            ExportScript.CheckFileExists = false;
+            ExportScript.CheckPathExists = true;
+            ExportScript.DefaultExt = "txt";
+            ExportScript.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            ExportScript.FileName = "0x"+mPacket.Opcode.ToString("X4");
+            ExportScript.FilterIndex = 2;
+            ExportScript.RestoreDirectory = true;
+            if(ExportScript.ShowDialog() == DialogResult.OK)
+            {
+                mScriptEditor.Export(ExportScript.FileName);
+                MessageBox.Show("Export success\r\nPath : "+ExportScript.FileName);
+            }
+        }
+
+        private void exportScriptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExportScript.InitialDirectory = @"C:\";
+            ExportScript.Title = "Expoer Mapleshark script";
+            ExportScript.CheckFileExists = false;
+            ExportScript.CheckPathExists = true;
+            ExportScript.DefaultExt = "txt";
+            ExportScript.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            ExportScript.FileName = "0x" + mPacket.Opcode.ToString("X4");
+            ExportScript.FilterIndex = 2;
+            ExportScript.RestoreDirectory = true;
+            if (ExportScript.ShowDialog() == DialogResult.OK)
+            {
+                mScriptEditor.Export(ExportScript.FileName);
+                MessageBox.Show("Export success\r\nPath : " + ExportScript.FileName);
             }
         }
     }
